9846c4da57428ea1e27a3c5a3ed57d8bd3604b24
Ridwan <ridwanraji4@gmail.com>
2022-10-20 14:55:34 +0100

    getting input from users

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const command = process.argv[2]
 
-console.log(process.argv) // logs out the argument passed as the third item of the argv array 
+console.log(process.argv)
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
