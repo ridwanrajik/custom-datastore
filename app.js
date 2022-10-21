b2f33253b4ed95b74d9ab5950d67adc96c82154b
Ridwan <ridwanraji4@gmail.com>
2022-10-21 10:04:07 +0100

    remove a datum from the database and format color with chalk.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs')
+const chalk = require('chalk')
 const data = require('./database.js')
 
 // Customize yargs version
@@ -7,15 +8,15 @@ yargs.version('1.1.0')
 // Create add command
 yargs.command({
     command: 'add',
-    describe: 'Add a new note',
+    describe: 'Add a new datum',
     builder: {
         title: {
-            describe: 'Note title',
+            describe: 'Datum title',
             demandOption: true,
             type: 'string'
         },
         body: {
-            describe: 'Note body',
+            describe: 'Datum body',
             demandOption: true,
             type: 'string'
         }
@@ -28,9 +29,16 @@ yargs.command({
 // Create remove command
 yargs.command({
     command: 'remove',
-    describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note')
+    describe: 'Remove a datum',
+    builder: {
+        title: {
+            describe: 'Datum title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        data.removeDatum(argv.title)
     }
 })
 
@@ -39,7 +47,7 @@ yargs.command({
     command: 'list',
     describe: 'List your notes',
     handler: function () {
-        console.log('Listing out all notes')
+        console.log(chalk.green.inverse('Listing out all notes'))
     }
 })
 
@@ -48,7 +56,7 @@ yargs.command({
     command: 'read',
     describe: 'Read a note',
     handler: function () {
-        console.log('Reading a note')
+        console.log(chalk.green.inverse('Reading a note'))
     }
 })
 
--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const addDatum = function (title, body) {
     const data = fetchData()
@@ -12,10 +13,24 @@ const addDatum = function (title, body) {
             body: body
         })
         saveData(data)
-        console.log('New data added!')
+        console.log(chalk.green.inverse('New data added!'))
     } else {
-        console.log('datum title taken!')
+        console.log(chalk.green.inverse('datum title taken!'))        
     }
+}
+
+const removeDatum = function (title) {
+    const data = fetchData()
+    const dataToKeep = data.filter(function (datum) {
+        return datum.title !== title
+    })
+
+    if (data.length > dataToKeep.length) {
+        console.log(chalk.green.inverse('Datum removed!'))
+        saveData(dataToKeep)
+    } else {
+        console.log(chalk.red.inverse('No data found!'))
+    }    
 }
 
 const saveData = function (data) {
@@ -34,5 +49,6 @@ const fetchData = function () {
 }
 
 module.exports = {
-    addDatum
+    addDatum,
+    removeDatum
 }
